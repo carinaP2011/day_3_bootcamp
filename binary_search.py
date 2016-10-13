67b2c2ae7637bc8a9654acaeaeaf972ec081f5c2
carinaP2011 <carinahappuch@gmail.com>
2016-10-13 06:59:01 +0300

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,31 +1,30 @@
 class BinarySearch():
-    def __init__(self,requiredlength=10,step=1):
-        self.listitems=[x for x in range(0+step,requiredlength*step,step)]
-        print(self.listitems)
-        self.length=requiredlength
-        #self.returndict=self.search(1)
-        self.first=0
-        self.last=self.length
-        self.mid=(self.first+self.last)//2
+    def __init__(self,a=10,b=1):
+        self.listitems=[x for x in range(0,a,b)]
+        self.length=a
+        self.firstitem=0
+        self.lastitem=self.length
+        self.mid=self.firstitem +self.lastitem//2
         self.found=False
         self.counter=0
-        self.dictvals=self.search(9)
-    def search(self,val):
-        #the binary search algorithm relies on having a sorted array of elements
-        #return number of iterations and where the number was found
-        while self.first<=self.last and not self.found:
-            self.mid=(self.first+self.last)//2
+        self.dictvals=self.search(1)
+    def binary_search(self,mylist):
+
+        while self.firstitem  <= self.lastitem and not self.found:
+            self.mid=self.firstitem+self.lastitem//2
             self.counter+=1
             if self.listitems[self.mid]==val:
                 self.found = True
             else:
                 if val<=self.listitems[self.mid]:
-                    self.last=self.mid
-                    self.search(val)#search the left half
+                    self.lastitem=self.mid
+                    self.search(mylist)
                 else:
-                    self.first=self.mid
-                    self.search(val)#search the right half
+                    self.firstitem=self.mid
+                    self.search(mylist)
 
         return {'count':self.counter,'index':self.mid}
-x=BinarySearch(20,2)
+x=binary_search
 print(x.dictvals)
+
+import unittest
